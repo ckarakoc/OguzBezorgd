534de81e8862ca2812f499ef2dd819c7fed26787
Celal Karakoc <c.karakoc@live.nl>
2024-11-05 19:40:43 +0100

    Added PrimeNG :tr:

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Aura } from 'primeng/themes/aura';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { definePreset, palette } from 'primeng/themes';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private config: PrimeNGConfig) {
+    const MyPreset = definePreset(Aura, {
+      semantic: {
+        primary: palette('{cyan}')
+      }
+    });
+
+    this.config.theme.set({
+      preset: MyPreset,
+      options: {
+        darkModeSelector: '.my-app-dark',
+      }
+    });
+  }
 }
